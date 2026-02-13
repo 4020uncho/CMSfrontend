602a1aa74269d4aa842f1ccfdd82d5950d9ca527
4020uncho <uncholimbu5@gmail.com>
2026-02-13 23:10:32 +0545

    first commit

---
+++ CMS frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return ( 
+    <div>
+      <h1>Unchohang limbu</h1>
+      <h3 style={{ color:"red" }}>i am a student </h3>
+      <h5>Age: {11+10}</h5>  
+    </div>
+  )
+}
+
+export default App
